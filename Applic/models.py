13475b0a6ddc8164d5fc91acb4dd75b75771816c
Alejandro Marquez <Alejandromarquez.alba@gmail.com>
2024-06-30 20:25:42 -0300

    panel

--- Applic/models.py
+++ Applic/models.py
@@ -6,11 +6,17 @@ from django.db import models
 class Market(models.Model):
     money = models.CharField(max_length=100)
     client = models.IntegerField()
+    def __str__(self):
+        return f'{self.money}'
 
 class Client(models.Model):
     money = models.CharField(max_length=100)
     product = models.IntegerField()
+    def __str__(self):
+        return f'{self.money}'
 
 class Worker(models.Model):
     pay = models.CharField(max_length=100)
     hours = models.IntegerField()
+    def __str__(self):
+        return f'{self.pay}'
